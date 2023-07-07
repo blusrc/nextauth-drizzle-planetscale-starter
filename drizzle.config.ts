c0e1c82e0dc92bdc359e99c842d863694430e00a
Aldiyar Serikov <ldr.stlt@gmail.com>
2023-07-07 16:49:55 +0600

    fix: configs, env, shadcn init

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from 'drizzle-kit';
-import 'dotenv/config';
+import "dotenv/config";
+import type { Config } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is missing');
+  throw new Error("DATABASE_URL is missing");
 }
 
 export default {
-  schema: './db/schema.ts',
-  out: './db/migrations',
-  driver: 'mysql2',
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
+  driver: "mysql2",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL,
   },
